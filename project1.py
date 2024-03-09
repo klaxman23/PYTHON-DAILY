03932d3e345f40ca8ce7e06693b17030f2f38e49
Laxman <laxmangbs1@gmail.com>
2024-03-09 21:57:25 +0530

    Initial commit

---
+++ project1.py
@@ -0,0 +1,11 @@
+# Write a program to print the age of the cadidate.
+
+name = input("Enter a name:")
+age = int(input("Enter your age: "))
+print(name)
+print(age)
+
+if age >18:
+    print("Yoy are eligible for vote!")
+else:
+    print(" Ho Sorry your are not eligible for vote!")
