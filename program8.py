019e16b3a9becda47c063a206511c53261ac616a
Laxman <laxmangbs1@gmail.com>
2024-03-10 11:13:40 +0530

    Factorial program

--- program8.py
+++ program8.py
@@ -1,7 +1,7 @@
 #Write a program to facotial.
 
 n  = int(input("Enter a value: "))
-mul = 1
+mull = 1
 for i in range(1,n+1):
-    mul *= i
-print("The Result:",mul)
+    mull *= i
+print("The Result:",mull)
