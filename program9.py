82f0400f29e3997ab6d7c86b4388ce9566b7ec92
Laxman <laxmangbs1@gmail.com>
2024-03-10 10:40:02 +0530

    odd and even numbers

---
+++ program9.py
@@ -0,0 +1,9 @@
+#Write a program list comperensive.
+
+print("====even number====")
+arr = [i for i in range(1,101) if i%2==0]
+print(arr)
+
+print("====odd number====")
+arr = [i for i in range(1,101) if i%2==1]
+print(arr)
