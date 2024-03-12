7e55c69b2f18f43a82912f20f9d1c0fa6c3062e5
Laxman <laxmangbs1@gmail.com>
2024-03-12 09:19:25 +0530

    #Write a program to print prime number from 1 to 100

---
+++ program13.py
@@ -0,0 +1,10 @@
+#Write a program to print prime number from 1 to 100
+
+num = 100
+
+for i in range(1,101):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        print(i)
