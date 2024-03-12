d1637470fb3451fae40b2be02b01dd86a1fc9e33
Laxman <laxmangbs1@gmail.com>
2024-03-12 09:15:38 +0530

    To remove dulicate values

--- program12.py
+++ program12.py
@@ -1,10 +1,13 @@
 #Write a program to remove the duplicate values?
 
+# arr = [1,1,2,2,3,3,4,4]
+# num = []
+# for i in arr:
+#     if i not in num:
+#         num.append(i)
+# print(num)
+
 arr = [1,1,2,2,3,3,4,4]
 
-num = []
-for i in arr:
-    if i not in num:
-        num.append(i)
-
+num = set(arr)
 print(num)
