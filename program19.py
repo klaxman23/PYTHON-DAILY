01d16b6f975d1005cae36fe278fcb55a2a9f231d
Laxman <laxmangbs1@gmail.com>
2024-03-23 22:45:47 +0530

    List programs

---
+++ program19.py
@@ -0,0 +1,22 @@
+#Write a  list programs
+
+#Empty list
+my_empty_list = []
+print(my_empty_list)
+my_list = [1,2,3,4,5,6]
+print(my_list)
+print(type(my_list))
+
+# positive index
+my_list = [1,2,3,4,5]
+print(my_list[2])
+
+# negative index
+new_list = [1,2,3,4,5]
+print(new_list[-1])
+
+#slicing
+new_list_1 = [11,22,33,44,55,66]
+print(new_list_1[4:])
+print(new_list_1[:4])
+
