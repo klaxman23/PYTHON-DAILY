4bd68f05340ab564b7e98dfcc4c05b6ae935e1b0
Laxman <laxmangbs1@gmail.com>
2024-07-30 21:48:35 +0530

    set new details added

--- program25.py
+++ program25.py
@@ -1,4 +1,17 @@
 #Write a small program with use of sets.
 thisset = {"apple", "banana", "cherry"}
-for i in thisset:
-    print(i)
+# for i in thisset:
+#     print(i)
+    
+# print("apple" in thisset)
+
+# thisset.add("orange")
+
+# print(thisset)
+
+
+tropical = {"sapota","mango"}
+
+thisset.update(tropical)
+
+print(thisset)
