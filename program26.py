06a7f54fd744eb24df467be3186683745483e0cb
Laxman <laxmangbs1@gmail.com>
2024-07-31 21:21:41 +0530

    set example programs

---
+++ program26.py
@@ -0,0 +1,20 @@
+#Write a program to add the set value to list values.
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+print(thisset)
+
+
+#Write a program set of items remove 1 item in the set.
+
+thisset = {"Orange","grapes","mango","apple"}
+
+thisset.remove("grapes")
+print(thisset)
+
+thisset.discard("grapess")
+print(thisset)
+
+
