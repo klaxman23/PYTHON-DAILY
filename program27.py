68916d16ba2afb4cd8ac0d29261b8a85a347707f
Laxman <laxmangbs1@gmail.com>
2024-07-31 21:52:30 +0530

    assign value program

---
+++ program27.py
@@ -0,0 +1,8 @@
+#Write a program to assign the value in the single line.
+
+
+name , age , roll_number = "Laxman",32,101
+
+print("candidate name:",name)
+print("candidate_age:",age)
+print("candidate_roll_no:",roll_number)
