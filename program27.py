4c9f3681fe88e92103936ee4b3dbf6da13f557ca
Laxman <laxmangbs1@gmail.com>
2024-08-01 21:42:41 +0530

    set example added

--- program27.py
+++ program27.py
@@ -1,7 +1,6 @@
 #Write a program to assign the value in the single line.
 
-
-name , age , roll_number = "Laxman",32,101
+name , age , roll_number = "Laxman", 32 , 101
 
 print("candidate name:",name)
 print("candidate_age:",age)
@@ -11,6 +10,6 @@ print("candidate_roll_no:",roll_number)
 
 name = "india"
 print("Length of the char:",len(name))
-print(name.find("d"))
+print(name.find("d")) # to find the char position.
 name1 = name[::-1] #reverse string.
 print(name1)
