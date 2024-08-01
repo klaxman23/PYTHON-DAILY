61020be2bace9ad30d6b540f0868cc7eaafd6527
Laxman <laxmangbs1@gmail.com>
2024-08-01 21:34:45 +0530

    reverse string

--- program27.py
+++ program27.py
@@ -6,3 +6,11 @@ name , age , roll_number = "Laxman",32,101
 print("candidate name:",name)
 print("candidate_age:",age)
 print("candidate_roll_no:",roll_number)
+
+#Write a program to print the length of the char.
+
+name = "india"
+print("Length of the char:",len(name))
+print(name.find("d"))
+name1 = name[::-1] #reverse string.
+print(name1)
