94fa8ada53fb5fb589f5ea28f2c2bc1b94d2f2f6
Laxman <laxmangbs1@gmail.com>
2024-08-02 22:16:20 +0530

    Loop examples

---
+++ loop_examples.py
@@ -0,0 +1,32 @@
+#Write a program for the range function.
+for i in range(11):
+    print(i)
+    
+else:
+    print("Sucessfully completed.")
+
+##############################################
+
+#Write a program to print the all the list of items.
+
+fruits = ["Banana","Orange","Grapes","Sapota","Apple"]
+
+for i in fruits:
+    print(i)
+###########################
+
+#Write a program to break the loop.
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+############################################
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+############################
+
+    
