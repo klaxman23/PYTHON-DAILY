e84d2a8c3f48331013e144a5a8c59fc2bac578e8
Laxman <laxmangbs1@gmail.com>
2024-08-03 21:26:17 +0530

    function examples!!

---
+++ fuctionexamples.py
@@ -0,0 +1,26 @@
+#Write a python program simple def the function and print the instruction.
+def my_program():
+    print("I love python program!!")
+
+my_program()
+##########################################################
+#Write a to print the welcome statement!!!
+
+def welcome_statement(fname):
+    print(fname + " "+ "Hello welcome!!!")
+    
+welcome_statement("Laxman")
+
+##############################################
+#Write a program print the fname and lname ?
+
+def name_details(fname,lname):
+    print("Welcome! ",fname + " "+ lname)
+    
+name_details("kadambala","Laxman")
+name_details("kadambala","Suma")
+name_details("kadambala","Nihal")
+
+     
+
+    
