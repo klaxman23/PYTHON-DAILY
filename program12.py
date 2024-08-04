b587f1d252d281b94d75a0265958d9c023517239
Laxman <laxmangbs1@gmail.com>
2024-08-04 11:57:18 +0530

    removed the duplicate values!!

--- program12.py
+++ program12.py
@@ -1,13 +1,34 @@
 #Write a program to remove the duplicate values?
 
-# arr = [1,1,2,2,3,3,4,4]
-# num = []
-# for i in arr:
-#     if i not in num:
-#         num.append(i)
-# print(num)
+arr = [1,1,2,2,3,3,4,4]
+num = []
+for i in arr:
+    if i not in num:
+        num.append(i)
+print(num)
 
 arr = [1,1,2,2,3,3,4,4]
 
 num = set(arr)
 print(num)
+
+
+def merge_array(arrayA,arrayB):
+    return(sorted(set(arrayA+  arrayB)))
+
+a = [1,2,3,3,4,5,6]
+b = [4,4,5,6,7,8,9]
+c = merge_array(a,b)
+print(c)
+#Write a program to remove the duplicate values.
+a = [1,2,3,3,4,5,6]
+b = [4,4,5,6,7,8,9]
+d = []
+c = []
+for i in a:
+    for j in b:
+      if i not in c:
+          if j not in d:
+           c.append(i)
+           d.append(j)
+print(c,d)
