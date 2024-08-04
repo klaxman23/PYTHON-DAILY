ddf0ab09c5780d74e52b01a077160310f25bbb75
Laxman <laxmangbs1@gmail.com>
2024-08-04 12:01:16 +0530

    function examples

--- fuctionexamples.py
+++ fuctionexamples.py
@@ -21,6 +21,33 @@ name_details("kadambala","Laxman")
 name_details("kadambala","Suma")
 name_details("kadambala","Nihal")
 
+#Write a program python
+
+def my_function(*kids):
+    print("Hello" + " " + kids[0])
+    print("Hello" + " " + kids[1])
+    print("Hello" + " " + kids[2])
+    print("Hello" + " " + kids[3])
+    
+my_function("Laxman","Suma","Kavya","Nihal")
+#Write a program python
+def mmy_function(food):
+    for i in food:
+        print(i)
+
+fruits = ("Apple","Orange","Grapes","Banana","Lemon")
+
+mmy_function(fruits)
+
+#Write a program for the table with use function
+
+def my_table(x):
+    return 5 * x
+
+print(my_table(5))
+
+
+
      
 
     
