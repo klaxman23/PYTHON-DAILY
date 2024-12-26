b300d5c845c74150ce4eec30816ba58d63df6649
Laxman <laxmangbs1@gmail.com>
2024-12-26 11:45:46 +0530

    all codes need to added

--- program30.py
+++ program30.py
@@ -1,7 +1,33 @@
-def even_odd(x):
-    if x%2==0:
-        print('it is even number')
-    elif x%2==1:
-        print('it is odd number')
+def compress_string(s):
+    """
+    Compresses a given string by counting consecutive occurrences of characters.
 
-even_odd(21)
+    Args:
+        s: The input string to be compressed.
+
+    Returns:
+        The compressed string.
+    """
+
+    compressed = ""
+    count = 1
+
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            count += 1
+        else:
+            compressed += s[i - 1] + str(count)
+            count = 1
+
+    compressed += s[-1] + str(count)  # Add the last character and its count
+
+    return compressed
+
+# Get input from the user
+input_string = input("Enter a string: ")
+
+# Compress the string
+compressed_string = compress_string(input_string)
+
+# Print the compressed string
+print("Compressed string:",compressed_string)
