d82048257ab4cf6d56e00ba74f616a2e3a55bc3f
Laxman <laxmangbs1@gmail.com>
2025-01-05 10:56:25 +0530

    New code added

---
+++ 05012025/pro1.py
@@ -0,0 +1,18 @@
+# Write a program to print the 1 to 100  numbers. and find the even and odd number
+
+# my_list = []
+# for i in range(1,101):
+#     if i%2==0:
+#         my_list.append(i)
+# print(my_list)
+
+my_arr = [i for i in range(1,111) if i%2==0]
+
+print(my_arr)
+
+
+my_arr = [i for i in range(1,111) if i%2==1]
+
+print(my_arr)
+
+
