27ed7b3a621d63f1785ebd039327c8276791791d
Laxman <laxmangbs1@gmail.com>
2025-02-24 12:59:38 +0530

    new codes added

--- 05012025/pro1.py
+++ 05012025/pro1.py
@@ -6,13 +6,19 @@
 #         my_list.append(i)
 # print(my_list)
 
-my_arr = [i for i in range(1,111) if i%2==0]
+# my_arr = [i for i in range(1,111) if i%2==0]
 
-print(my_arr)
+# print(my_arr)
 
 
-my_arr = [i for i in range(1,111) if i%2==1]
+# my_arr = [i for i in range(1,111) if i%2==1]
 
-print(my_arr)
+# print(my_arr)
+
+#Write a program to print the tables.
+num = int(input("Enter a value: "))
+for i in range (1,11):
+    print(num ,'*',i,'=',num*i)
+
 
 
---
+++ 05012025/pro2.py
@@ -0,0 +1,10 @@
+for x in range(1,10,1):
+    print(x)
+    
+#Write a program to print the sum of the n numbers.
+
+num = int(input("Enter a value: "))
+sum = 0
+for i in range(1,num+1):
+    sum = sum+i
+print(sum)
