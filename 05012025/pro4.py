b2ccce2c516d110fa8a2641d85617454a048623b
Laxman <laxmangbs1@gmail.com>
2025-03-02 22:11:32 +0530

    For loop 5 codes added

---
+++ 05012025/pro4.py
@@ -0,0 +1,33 @@
+# Write a programs on for loop (5 programs)
+
+# Print 1 to n
+
+# n = 25
+# for i in range(1,n+1):
+#     print(i , end=" ")
+    
+# Print even number up to n
+
+# v = 10
+# for i in range(2,v+1,2):
+#     print(i , end=" ")
+    
+# Print odd number up to n
+
+# x = 10
+# for i in range(1,x+1,2):
+#     print(i , end=" ")
+
+
+# print n even number.
+
+# v = 10
+# for i in range(1,v+1):
+#     print(i*2 , end=" ")
+    
+# print n even number.
+
+# v = 10
+# for i in range(1,v+1):
+#     print(i*2-1 , end=" ")
+
